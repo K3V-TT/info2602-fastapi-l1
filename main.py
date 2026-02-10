2ea0f7300740cd5710423a52fe6c4acffd8b8b4f
K3V-TT <kjaydon11@gmail.com>
2026-02-09 22:18:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,23 @@ with open('./data.json') as f:
 
 
 @app.get('/')
-def hello_world():
+async def hello_world():
     return 'Hello, World!'
+
+### New Function
+@app.get('/students')
+async def get_students(pref=None):
+    if pref:
+        filtered_students = []
+        for student in data:
+            if student['pref'] == pref: # select only the students with a given meal preference
+              filtered_students.append(student) # add match student to the result
+        return filtered_students
+    return data
+### End of new function
+
+@app.get('/students/{id}')
+async def get_student(id):
+  for student in data: 
+    if student['id'] == id: # Only return the student if the ID matches
+      return student
